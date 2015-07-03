4298d63127793c7f7ff0eef6cad1647c9d56cd42
21MountainMan <johnathonwalker@icloud.com>
2015-07-03 11:47:46 -0600

    added ACCompanion class added several raw assets: lists of in-game items

---
+++ app/src/main/java/com/dgjw/acc/ACCompanion.java
@@ -0,0 +1,20 @@
+package com.dgjw.acc;
+
+import android.os.Parcelable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by johnathon on 7/2/2015.
+ */
+public class ACCompanion {
+
+    ArrayList<Parcelable> items = new ArrayList<>();
+
+    private void loadData() throws IOException, NullPointerException {
+
+    }
+}
